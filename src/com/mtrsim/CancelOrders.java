90939f824c217d98aae1dfbb3db072c307fecf92
Navoda <navomails@gmail.com>
2016-12-21 22:16:49 +0300

    Added logger for CancelOrders

--- src/com/mtrsim/CancelOrders.java
+++ src/com/mtrsim/CancelOrders.java
@@ -1,7 +1,11 @@
 package com.mtrsim;
 
+import org.apache.log4j.Logger;
+
 /**
  * Created by Navoda on 12/19/2016.
  */
 public class CancelOrders {
+
+    final static Logger logger = Logger.getLogger(ChangeOrders.class);
 }
